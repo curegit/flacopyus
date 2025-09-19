c94e46492903a204c8dc7e5af63e425d9892eaa9
curegit <37978051+curegit@users.noreply.github.com>
2025-09-19 21:41:17 +0900

    --delete

--- flacopyus/cli.py
+++ flacopyus/cli.py
@@ -18,8 +18,16 @@ def main(argv: list[str] | None = None) -> int:
         parser.add_argument("-v", "--version", action="version", version=version)
         parser.add_argument("src", metavar="SRC", type=str, help="source directory")
         parser.add_argument("dest", metavar="DEST", type=str, help="destination directory")
+        group = parser.add_mutually_exclusive_group()
+        group.add_argument("--delete", action="store_true", help="")
+        group.add_argument("--delete-excluded", action="store_true", help="")
         args = parser.parse_args(argv)
-        return main_func(Path(args.src), Path(args.dest))
+        return main_func(
+            src=Path(args.src),
+            dest=Path(args.dest),
+            delete=(args.delete or args.delete_excluded),
+            delete_excluded=args.delete_excluded,
+        )
 
     except KeyboardInterrupt:
         eprint("KeyboardInterrupt")
