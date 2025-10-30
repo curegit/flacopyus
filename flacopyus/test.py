3cfeb7b4cb0cf789672b17b427649d0dc45c09ec
curegit <37978051+curegit@users.noreply.github.com>
2025-10-30 12:58:59 +0900

    Add test

--- flacopyus/test.py
+++ flacopyus/test.py
@@ -3,8 +3,7 @@ from .opus import OpusOptions, build_opusenc_func
 
 def main(
     *,
-    opus_options: OpusOptions = OpusOptions(),
     verbose: bool = False,
 ) -> int:
-    f = build_opusenc_func(opus_options)
+    f = build_opusenc_func(OpusOptions())
     return 0
